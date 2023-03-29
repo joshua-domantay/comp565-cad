f73fba572a5a4167219d91f44f9e6c725db51749
Jonathan <jon.chua51@gmail.com>
2023-03-28 17:17:35 -0700

    Added GameObjects [Cube Wood, Cuboid 4x4xL, TMP InputField in Canvas], Added Scripts [CuboidFactory4x4, CuboidLengthSetter]

---
+++ Assets/Scripts/CuboidFactory4x4.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CuboidFactory4x4 : MonoBehaviour
+{
+    public GameObject cuboidPrefab;
+    public float length;
+
+    void Start()
+    {
+        Vector3 position = new Vector3(1.5f, length / 2, 1.5f);
+        GameObject cuboid = Instantiate(cuboidPrefab, position, Quaternion.identity);
+        cuboid.transform.localScale = new Vector3(4, length, 4);
+    }
+}
---
+++ Assets/Scripts/CuboidLengthSetter.cs
@@ -0,0 +1,15 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CuboidLengthSetter : MonoBehaviour
+{
+    public CuboidFactory4x4 cuboidFactory;
+    public TMP_InputField inputField;
+
+    public void SetCuboidLength()
+    {
+        float length = float.Parse(inputField.text);
+        cuboidFactory.length = length;
+    }
+}
