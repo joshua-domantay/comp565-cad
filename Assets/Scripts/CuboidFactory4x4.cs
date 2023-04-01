7e65d9a8e7dd1c366b3e1e0feac93877cf7c31cb
Joshua Anthony Domantay <dom.josh2701@gmail.com>
2023-03-31 23:23:28 -0700

    Add Object tag for cuboids. Hide Cube Wood in scene

--- Assets/Scripts/CuboidFactory4x4.cs
+++ Assets/Scripts/CuboidFactory4x4.cs
@@ -17,6 +17,7 @@ public class CuboidFactory4x4 : MonoBehaviour
     public void CreateCuboid(float cLength) {
         Vector3 position = new Vector3(1.5f, cLength / 2, 1.5f);
         GameObject cuboid = Instantiate(cuboidPrefab, position, Quaternion.identity);
+        cuboid.tag = "Object";
         cuboid.transform.localScale = new Vector3(4, cLength, 4);
     }
 }
