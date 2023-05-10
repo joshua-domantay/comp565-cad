a988444f7a003d54cf28c1474968da7d3465ef87
Jonathan <jon.chua51@gmail.com>
2023-05-10 14:41:43 -0700

    updated the PMScript

--- Assets/Scripts/PhotonManagerScript.cs
+++ Assets/Scripts/PhotonManagerScript.cs
@@ -17,19 +17,35 @@ public class PhotonManagerScript : MonoBehaviourPunCallbacks
     public override void OnConnectedToMaster()
     {
         Debug.Log("Connected to Master");
-        PhotonNetwork.JoinRandomRoom();
     }
 
-
-    public override void OnJoinRandomFailed(short returnCode, string message)
+    public void CreateRandomRoom()
     {
-        Debug.Log("Failed to join random room,creating a new room");
-        PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = 4 });
+        string roomName = Random.Range(100000000, 999999999).ToString();
+        Debug.Log($"Room #{roomName} created");
+        PhotonNetwork.CreateRoom(roomName, new RoomOptions { MaxPlayers = 4 });
+    }
+
+    public void JoinRoomByName(string roomName)
+    {
+        PhotonNetwork.JoinRoom(roomName);
     }
 
     public override void OnJoinedRoom()
     {
         Debug.Log("Joined room");
         PhotonNetwork.Instantiate(playerPrefab.name, playerPrefab.transform.position, playerPrefab.transform.rotation);
+    }
+
+    public override void OnCreatedRoom()
+    {
+        Debug.Log("Room created");
+        PhotonNetwork.Instantiate(playerPrefab.name, playerPrefab.transform.position, playerPrefab.transform.rotation);
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("Failed to join random room, creating a new room");
+        CreateRandomRoom();
     }
 }
